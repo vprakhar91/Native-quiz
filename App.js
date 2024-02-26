3a365ee1c92cab631977f796b72f3292abc66e71
Prakhar Verma <46259033+vprakhar91@users.noreply.github.com>
2024-02-26 11:03:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import StackNavigator from './StackNavigator';
+
+export default function App() {
+  return (
+   <StackNavigator/>
+  );
+}
+
